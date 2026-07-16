67a0fe2d673c0d2f49406980d60bf9c323cb6090
lightyears <lightyears1998@hotmail.com>
2026-07-17 01:01:14 +0800

    refactor: Migrate ActivatableExtensions to use standard ReactiveUI interfaces

--- QfStudio.Godette.ReactiveUI/ActivatableExtensions.cs
+++ QfStudio.Godette.ReactiveUI/ActivatableExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System.Reactive.Disposables;
+using System.Reactive.Disposables;
 using Godot;
 using System.ComponentModel;
 using System.Linq.Expressions;
@@ -10,85 +10,75 @@ using ReactiveUI;
 
 namespace QfStudio.Godette.ReactiveUI;
 
+// TODO migrate to GodotPropertyBindingHook
 public static class ActivatableExtensions
 {
-    extension(IActivatable activation)
+    public static IDisposable Bind<TModel, TProperty>(TModel model, Expression<Func<TModel, TProperty>> propExpr, Godot.Range view)
+        where TModel : INotifyPropertyChanged
+        where TProperty : INumber<TProperty>
     {
-        public IDisposable Bind<TModel, TProperty>(TModel model, Expression<Func<TModel, TProperty>> propExpr, Godot.Range view)
-            where TModel : INotifyPropertyChanged
-            where TProperty : INumber<TProperty>
-        {
-            var disposable = new CompositeDisposable();
+        var disposable = new CompositeDisposable();
+        var setter = CompileSetter(propExpr);
 
-            var memberExpr = (MemberExpression)propExpr.Body;
-            var propInfo = (PropertyInfo)memberExpr.Member;
+        Observable.FromEvent<Godot.Range.ValueChangedEventHandler, double>(
+                h => view.ValueChanged += h,
+                h => view.ValueChanged -= h)
+            .Subscribe(v => setter(model, TProperty.CreateSaturating(v)))
+            .DisposeWith(disposable);
 
-            Observable.FromEvent<Godot.Range.ValueChangedEventHandler, double>(
-                    h => view.ValueChanged += h,
-                    h => view.ValueChanged -= h)
-                .Subscribe(v =>
-                {
-                    propInfo.SetValue(model, v);
-                })
-                .DisposeWith(disposable);
+        model.WhenAnyValue(propExpr)
+            .ObserveOn(RxSchedulers.MainThreadScheduler)
+            .Subscribe(v => view.Value = double.CreateSaturating(v))
+            .DisposeWith(disposable);
 
-            model.WhenAnyValue(propExpr)
-                .ObserveOn(activation.UiContext)
-                .Subscribe(v =>
-                {
-                    view.Value = double.CreateSaturating<TProperty>(v);
-                })
-                .DisposeWith(disposable);
+        return disposable;
+    }
 
-            return disposable;
-        }
+    public static IDisposable BindTo<TModel, TProperty>(TModel model, Expression<Func<TModel, TProperty>> propExpr, Godot.Label view)
+        where TModel : INotifyPropertyChanged
+        where TProperty : IConvertible
+    {
+        var disposable = new CompositeDisposable();
 
-        public IDisposable BindTo<TModel, TProperty>(TModel model, Expression<Func<TModel, TProperty>> propExpr, Godot.Label view)
-            where TModel : INotifyPropertyChanged
-            where TProperty : IConvertible
-        {
-            var disposable = new CompositeDisposable();
+        model.WhenAnyValue(propExpr)
+            .ObserveOn(RxSchedulers.MainThreadScheduler)
+            .Subscribe(v => view.Text = v.ToString())
+            .DisposeWith(disposable);
 
-            model.WhenAnyValue(propExpr)
-                .ObserveOn(activation.UiContext)
-                .Subscribe(v =>
-                {
-                    view.Text = v.ToString();
-                })
-                .DisposeWith(disposable);
+        return disposable;
+    }
 
-            return disposable;
-        }
+    public static IDisposable Bind<TModel, TProperty>(TModel model, Expression<Func<TModel, TProperty>> propExpr, Godot.LineEdit view)
+        where TModel : INotifyPropertyChanged
+        where TProperty : IConvertible
+    {
+        var disposable = new CompositeDisposable();
+        var setter = CompileSetter(propExpr);
 
-        public IDisposable Bind<TModel, TProperty>(TModel model, Expression<Func<TModel, TProperty>> propExpr, Godot.LineEdit view)
-            where TModel : INotifyPropertyChanged
-            where TProperty : IConvertible
-        {
-            var disposable = new CompositeDisposable();
+        Observable.FromEvent<Godot.LineEdit.TextChangedEventHandler, string>(
+                h => view.TextChanged += h,
+                h => view.TextChanged -= h)
+            .Subscribe(v => setter(model, (TProperty)Convert.ChangeType(v, typeof(TProperty))))
+            .DisposeWith(disposable);
 
-            var memberExpr = (MemberExpression)propExpr.Body;
-            var propInfo = (PropertyInfo)memberExpr.Member;
+        model.WhenAnyValue(propExpr)
+            .ObserveOn(RxSchedulers.MainThreadScheduler)
+            .Where(v => view.Text != v.ToString())
+            .Subscribe(v => view.Text = v.ToString())
+            .DisposeWith(disposable);
 
-            Observable.FromEvent<Godot.LineEdit.TextChangedEventHandler, string>(
-                    h => view.TextChanged += h,
-                    h => view.TextChanged -= h)
-                .Subscribe(v =>
-                {
-                    GD.Print("text changed!");
-                    propInfo.SetValue(model, v);
-                })
-                .DisposeWith(disposable);
+        return disposable;
+    }
 
-            model.WhenAnyValue(propExpr)
-                .ObserveOn(activation.UiContext)
-                .Where(v => view.Text != v.ToString())
-                .Subscribe(v =>
-                {
-                    view.Text = v.ToString();
-                })
-                .DisposeWith(disposable);
-
-            return disposable;
-        }
+    private static Action<TModel, TProperty> CompileSetter<TModel, TProperty>(Expression<Func<TModel, TProperty>> propExpr)
+    {
+        var memberExpr = (MemberExpression)propExpr.Body;
+        var propInfo = (PropertyInfo)memberExpr.Member;
+        var param = System.Linq.Expressions.Expression.Parameter(typeof(TModel), "obj");
+        var valueParam = System.Linq.Expressions.Expression.Parameter(typeof(TProperty), "value");
+        return System.Linq.Expressions.Expression.Lambda<Action<TModel, TProperty>>(
+            System.Linq.Expressions.Expression.Assign(
+                System.Linq.Expressions.Expression.Property(param, propInfo), valueParam),
+            param, valueParam).Compile();
     }
 }
